b26264ef821a953b9a5106a94372a78e52a80822
t0nqwer <oottoonnoo@gmail.com>
2023-09-30 22:29:25 +0700

    master

--- src/assets/public.js
+++ src/assets/public.js
@@ -1,8 +1,8 @@
 // export const url = "https://khwantaqwe-zfn2h52c7a-uc.a.run.app";
 // export const url = "https://khwanta-zfn2h52c7a-as.a.run.app/";
 // export const url = "https://khwanta-api2546.com";
-// export const url = "http://localhost:7070";
-export const url = "http://192.168.0.252:7070";
+export const url = "http://localhost:7070";
+// export const url = "http://192.168.0.252:7070";
 
 const lableClassName = "max-lg:w-32 w-52 text-right";
 const divclass =
