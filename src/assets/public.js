963410f160ba0bf24c6b6e9b7b486182dc7aab4b
t0nqwer <oottoonnoo@gmail.com>
2024-02-08 15:07:58 +0700

    master

--- src/assets/public.js
+++ src/assets/public.js
@@ -1,15 +1,14 @@
 // export const url = "https://khwantaqwe-zfn2h52c7a-uc.a.run.app";
 // export const url = "https://khwanta-zfn2h52c7a-as.a.run.app/";
 // export const url = "https://khwanta-api2546.com";
-// export const url = "http://localhost:7070";
-export const url = "http://192.168.0.252:7070";
+export const url = "http://localhost:7070";
+// export const url = "http://192.168.0.252:7070";
 // export const url = "http://157.245.195.154";
 // export const url = "https://khwanta.site";
 
 const lableClassName = "max-lg:w-32 w-52 text-right";
 const divclass =
   "flex w-1/2 max-lg:w-full py-3 items-center space-x-4 text-lg relative";
-
 export const weights = {
   XXS: 1,
   XS: 2,
